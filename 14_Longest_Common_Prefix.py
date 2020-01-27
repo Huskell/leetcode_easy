309c9accc88db06655935365c9a4b56591d838e9
Huskell <mihaylov.ne@gmail.com>
2020-01-27 04:58:44 +0300

    Leetcode, easy: problem 14

--- 14_Longest_Common_Prefix.py
+++ 14_Longest_Common_Prefix.py
@@ -20,6 +20,20 @@ class Solution:
                     break
         return prefix
 
+    def longestCommonPrefix_2(self, strs: List[str]) -> str:
+        if strs == []:
+            return ""
+        prefix = str()
+        for i in zip(*strs):
+            if len(set(i)) == 1: # set - удаляет все похожие элементы (т.е. не содержит повторов)
+                prefix += i[0]
+            else:
+                break
+
+        return prefix
 if __name__ == '__main__':
     sol = Solution().longestCommonPrefix(["flower","flow","flight"])
+    sol2 = Solution().longestCommonPrefix_2(["flower","flow","flight"])
     print(sol)
+    print(sol2)
+    #print(int('str') - int('stre'))
