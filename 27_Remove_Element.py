3d94cb93ad48bc8aeb64708d5dd338ac403f81d2
Huskell <mihaylov.ne@gmail.com>
2020-02-01 15:58:37 +0300

    Leetcode, easy: problem 27

---
+++ 27_Remove_Element.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        c = nums.count(val)
+        for i in range(c):
+            nums.remove(val)
+
+        return len(nums)
+
+    def removeElement2(self, nums, val):
+        try:
+            while True:
+                nums.remove(val)
+        except:
+            return len(nums)
+
+if __name__ == '__main__':
+    s = [1,2,2,4,5]
+    val = 2
+    print(Solution().removeElement(s, val))
